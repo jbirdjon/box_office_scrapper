740ef6e252108320a0f252ec2f7dc96d1025a89a
jbirdjon <jonathanwallis@my.unt.edu>
2019-12-19 00:58:14 -0600

    everything works how it is supposed to, just need to add a timer on it

--- scrape.py
+++ scrape.py
@@ -6,10 +6,12 @@ import requests #
 from bs4 import BeautifulSoup # also known as 'beautifulscraper'
 from apscheduler.schedulers.blocking import BlockingScheduler # scheduler
 from datetime import datetime # this will let us access current time
-# sched = BlockingScheduler();
+sched = BlockingScheduler();
 
 def main():
-    
+    phoneNumber = 5126578867
+    # return list to append values in message to
+    ret = []
     # get url for the page somehow here. use datetime to calculate the week
     year = datetime.now().year
     weekNum = datetime.now().isocalendar()[1]
@@ -24,17 +26,21 @@ def main():
     for x in range(5): # this loops through each attribute in the top 5 values.
         rank = soup.find_all("td", class_="a-text-right mojo-header-column mojo-truncate mojo-field-type-rank mojo-sort-column")[x].get_text() # get the rank for the week
         title = soup.find_all("td", class_="a-text-left mojo-field-type-release mojo-cell-wide")[x].get_text() # get the title of the film
-        gross = soup.find_all("td", class_="a-text-right mojo-field-type-money mojo-estimatable")[x].get_text() # get gross for the weekend
-        total_gross = soup.find_all("td", class_="a-text-right mojo-field-type-money mojo-estimatable")[x].get_text() # get total gross
+        gross = soup.find_all("td", class_="a-text-right mojo-field-type-money mojo-estimatable")[(3*x)].get_text() # get gross for the weekend
+        total_gross = soup.find_all("td", class_="a-text-right mojo-field-type-money mojo-estimatable")[(x*3)+2].get_text() # get total gross
         weeks = soup.find_all("td", class_="a-text-right mojo-field-type-positive_integer")[(x*2)+1].get_text() # get number of weeks it's been in studio
 
-        # just formatting english language stuff
-
+        # appends values to return value
         if int(weeks) == 1:
-            print rank + ": " + str(title) + " made " + str(gross) + " dollars in it's opening weekend."
+            ret.append(rank + ": " + str(title) + " made " + str(gross) + " dollars in its opening weekend.")
         else:
-            print rank + ": " + str(title) + " made " + str(gross) + " dollars last week. It has made " + str(total_gross) + " dollars over " + str(weeks) + " weeks"
+            ret.append(rank + ": " + str(title) + " made " + str(gross) + " dollars last week. It has made " + str(total_gross) + " dollars over " + str(weeks) + " weeks")
+        ret.append("\n")
 
+    # joins all the values into a string that it then prints
+    message = ''.join(ret)
+    cmd = "osascript sendMessage.scpt " + str(phoneNumber) + " '" + str(message) + "' "
+    os.system(cmd)
 
 if __name__ == "__main__":
     main()
